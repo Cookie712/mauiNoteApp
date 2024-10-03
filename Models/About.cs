00d273c329df96fc6f3706ab818b74d17acc5fb7
Cookie712 <cookie7122006@gmail.com>
2024-10-03 09:00:03 +0200

    maui first app

---
+++ Models/About.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiApp1.Models
+{
+    class About
+    {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://aka.ms.maui";
+        public string Message => "This app is written in .NET MAUI";
+    }
+}
---
+++ Models/AllNotes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiApp1.Models
+{
+    internal class AllNotes
+    {
+        public ObservableCollection<Note> Notes { get; set; } = new();
+
+        public AllNotes() => LoadNotes();
+
+        public void LoadNotes()
+        {
+            Notes.Clear();
+            string appDataPath = FileSystem.AppDataDirectory;
+
+            IEnumerable<Note> notes = Directory.
+                EnumerateFiles(appDataPath, "*.notes.txt").
+                Select(fileName => new Note()
+                {
+                    Filename = fileName,
+                    Text = File.ReadAllText(fileName),
+                    Date = File.GetLastWriteTime(fileName)
+                })
+                .OrderBy(note => note.Date);
+
+            foreach (Note note in notes)
+            {
+                Notes.Add(note);
+            }
+        }
+    }
+}
